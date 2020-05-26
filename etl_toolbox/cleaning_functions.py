cee4c0bdeae0b9f62a2e9d4efdef7f8e1d28628e
brooklyn <ludlow.22@osu.edu>
2020-05-26 03:37:51 -0400

    Write clean_whitespace() description and unit tests

--- etl_toolbox/cleaning_functions.py
+++ etl_toolbox/cleaning_functions.py
@@ -4,3 +4,24 @@ import re
 def fingerprint(x):
     """Returns a lowercase, alphanumeric representation of x"""
     return re.sub(r'[^0-9a-z]', '', str(x).lower())
+
+
+def clean_whitespace(x):
+    """
+    Returns x with:
+     - all whitespace characters replaced with standard ASCII space (32)
+     - consecutive whitespace condensed
+     - leading/trailing whitespace removed
+
+    Example input: ''' 123   abc 456
+                                def\t\t 789\t'''
+    Example output: '123 abc 456 def 789'
+
+    Expects: str
+    Returns: str
+
+    Note: Does not remove unicode formatting characters without White_Space property:
+     - \u180E \u200B \u200C \u200D \u2060 \uFEFF
+    """
+
+    pass
--- tests/test_cleaning_functions.py
+++ tests/test_cleaning_functions.py
@@ -1,5 +1,5 @@
 import pytest
-from etl_toolbox.cleaning_functions import fingerprint
+from etl_toolbox.cleaning_functions import fingerprint, clean_whitespace
 
 
 @pytest.mark.parametrize("input, expected", [
@@ -10,3 +10,24 @@ from etl_toolbox.cleaning_functions import fingerprint
 ])
 def test_fingerprint(input, expected):
     assert fingerprint(input) == expected
+
+
+@pytest.mark.parametrize("input, expected", [
+    (''' 123   abc 456
+            def\t\t 789\t''',   '123 abc 456 def 789'),
+    ('   spaces   ',            'spaces'),
+    ('\t\n\r\f\v',              ''),
+    (u'\u2008 and \u3000',      'and'),
+    (u'''\u0009 \u000A \u000B
+        \u000C \u000D a \u0020 
+        \u0085 \u00A0 \u1680
+        \u2000 \u2001 \u2002 b
+        \u2003 \u2004 \u2005
+        \u2006 \u2007 \u2008
+        \u2009 \u200A \u2028 c
+        \u2029 \u202F \u205F
+        \u3000''',              'a b c'),
+    ('',                        '')
+])
+def test_clean_whitespace(input, expected):
+    assert clean_whitespace(input) == expected

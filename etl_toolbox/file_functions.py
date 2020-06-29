6fdb0e88bfd243f6da09a4ad98262a81a011ce5f
brooklyn <ludlow.22@osu.edu>
2020-06-29 11:06:25 -0400

    Add get_file_list_from_dir() and unit tests

---
+++ etl_toolbox/file_functions.py
@@ -0,0 +1,56 @@
+import os
+import re
+
+
+def get_file_list_from_dir(dir, recursive=False, include_regex=None):
+    """
+    Returns a `list` of the files in a directory
+
+    Example:
+      >>> get_file_list_from_dir('test_data/test_dir')
+      ['test_data/test_dir/1.csv',
+       'test_data/test_dir/2.csv',
+       'test_data/test_dir/3.json']
+
+    :param recursive:
+        (optional) If set to ``True``, the returned `list` will include files
+        from ``dir`` and all of its subdirectories. Default is ``False``.
+
+        Example:
+          >>> get_file_list_from_dir('test_data/test_dir', recursive=True)
+          ['test_data/test_dir/1.csv',
+           'test_data/test_dir/2.csv',
+           'test_data/test_dir/3.json',
+           'test_data/test_dir/a/1.csv',
+           'test_data/test_dir/b/3.csv',
+           'test_data/test_dir/b/c/2.txt']
+
+    :param include_regex:
+        (optional) Only include files whose path matches this regex. Default
+        is ``None`` (list is unfiltered).
+
+        Example:
+          >>> get_file_list_from_dir('test_data/test_dir',
+                                     include_regex=r'.*\.csv$')
+          ['test_data/test_dir/1.csv',
+           'test_data/test_dir/2.csv']
+
+    :return:
+        Returns `list` of file paths.
+    """
+
+    file_list = []
+
+    # Collect file paths
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            file_list.append(os.path.join(root, f))
+
+        if not recursive:
+            break
+
+    # Apply filter
+    if include_regex is not None:
+        file_list = [f for f in file_list if re.match(include_regex, f)]
+
+    return file_list
---
+++ tests/test_file_functions.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+from etl_toolbox.file_functions import get_file_list_from_dir
+
+
+@pytest.mark.parametrize("dir, recursive, include_regex, expected", [
+    (
+        os.path.join('test_data', 'test_dir'),
+        False,
+        None,
+        [os.path.join('test_data', 'test_dir', '1.csv'),
+         os.path.join('test_data', 'test_dir', '2.csv'),
+         os.path.join('test_data', 'test_dir', '3.json')]
+        ),
+    (
+        os.path.join('test_data', 'test_dir'),
+        True,
+        None,
+        [os.path.join('test_data', 'test_dir', '1.csv'),
+         os.path.join('test_data', 'test_dir', '2.csv'),
+         os.path.join('test_data', 'test_dir', '3.json'),
+         os.path.join('test_data', 'test_dir', 'a', '1.csv'),
+         os.path.join('test_data', 'test_dir', 'b', '3.csv'),
+         os.path.join('test_data', 'test_dir', 'b', 'c', '2.txt')]
+        ),
+    (
+        os.path.join('test_data', 'test_dir'),
+        False,
+        r'.*\.json$',
+        [os.path.join('test_data', 'test_dir', '3.json')]
+        ),
+    (
+        os.path.join('test_data', 'test_dir'),
+        True,
+        r'.*2\..*',
+        [os.path.join('test_data', 'test_dir', '2.csv'),
+         os.path.join('test_data', 'test_dir', 'b',  'c', '2.txt')]
+        )
+])
+def test_get_file_list_from_dir(dir, recursive, include_regex, expected):
+    assert get_file_list_from_dir(dir,
+                                  recursive=recursive,
+                                  include_regex=include_regex) == expected

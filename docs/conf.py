9bc96ffd9bb271139163a105b9c0f1be673af335
brooklyn <ludlow.22@osu.edu>
2020-09-23 20:59:41 -0400

    Add sphinx config files for generating docs

---
+++ docs/conf.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+
+import sphinx_bootstrap_theme
+
+sys.path.insert(0, os.path.abspath(".."))
+
+import etl_toolbox
+
+# -- Project information -----------------------------------------------------
+
+project = u'etl-toolbox 🧰'
+copyright = '2020, Brooklyn Rose Ludlow'
+author = 'Brooklyn Rose Ludlow'
+
+release = etl_toolbox.__version__
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+]
+
+language = 'en'
+
+pygments_style = "friendly"
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for sphinx extensions -------------------------------------------
+
+# Looks for objects in external projects
+intersphinx_mapping = {
+    "pandas": (
+        "https://pandas.pydata.org/pandas-docs/stable/",
+        "https://pandas.pydata.org/pandas-docs/stable/objects.inv",
+    ),
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_static_path = []
+
+html_sidebars = {'**': ['localtoc.html']}
+
+html_show_sourcelink = False
+html_show_sphinx = False
+html_show_copyright = True
+
+html_theme_options = {
+    # Theme setting (from https://bootswatch.com/)
+    'bootswatch_theme': "yeti",
+
+    # Render the next and previous page links in navbar. (Default: true)
+    'navbar_sidebarrel': False,
+
+    # Render the current pages TOC in the navbar. (Default: true)
+    'navbar_pagenav': False,
+
+    # Include hidden toctree links (Default: true)
+    'globaltoc_includehidden': "false",
+
+    # Custom navbar links
+    'navbar_links': [
+        ("Overview", "index"),
+        ("Modules & Functions", "modules"),
+        # ("Link", "http://example.com", True),
+    ],
+}
---
+++ docs/modules.rst
@@ -0,0 +1,44 @@
+Modules & functions
+===================
+
+..
+
+____________________________
+
+Cleaning Functions
+---------------------------------------
+
+.. automodule:: etl_toolbox.cleaning_functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+____________________________
+
+DataFrame Functions
+----------------------------------------
+
+.. automodule:: etl_toolbox.dataframe_functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+____________________________
+
+File Functions
+-----------------------------------
+
+.. automodule:: etl_toolbox.file_functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+____________________________
+
+Mapping Functions
+--------------------------------------
+
+.. automodule:: etl_toolbox.mapping_functions
+   :members:
+   :undoc-members:
+   :show-inheritance:
